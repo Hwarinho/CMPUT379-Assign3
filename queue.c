1aad906f6462ec93ba8e45b2eec5dc468f56bf00
Oleg Krushelnytskyy <oleg@ualberta.ca>
2019-04-08 13:41:31 -0600

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,5 +1,5 @@
 //LinkedHashMap was NOT developed by oleg or nikita.
-//We found the LinkedHashMap on: https://github.com/carlossdparaujo/linked-hash-map
+//We found the LinkedHashMap on: https://www.geeksforgeeks.org/lru-cache-implementation/
 //Use of outside code is approved by Ioanis Nikolaidis.
 
 #include <stdio.h>
@@ -7,10 +7,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <getopt.h>
-#include "linkedhashmap.h"
 
 const char* policy;
-const char* flushPeriod;
+int flushPeriod;
 int pgsize;
 int tblsize;
 int tlbHitCount;
@@ -21,9 +20,152 @@ char buf[100] = {0};
 char addr_str[9] = {0};
 unsigned int addr;
 
-/*void printSTRUCT(ELEMENT *element){
-    printf("KEY: %d ||| VALUE: %f",element->key,element->value);
-}*/
+// A Queue Node (Queue is implemented using Doubly Linked List) 
+typedef struct QNode{ 
+    struct QNode *prev, *next; 
+    unsigned pageNumber;
+} QNode; 
+
+// A Queue (A FIFO collection of Queue Nodes) 
+typedef struct Queue{ 
+    unsigned count;  
+    unsigned numberOfFrames; 
+    QNode *front, *rear; 
+} Queue; 
+  
+// A hash (Collection of pointers to Queue Nodes) 
+typedef struct Hash{ 
+    int capacity; 
+    QNode* *array;
+} Hash;
+
+QNode* newQNode( unsigned pageNumber){ 
+    QNode* temp = (QNode *)malloc( sizeof( QNode ) ); 
+    temp->pageNumber = pageNumber; 
+
+    temp->prev = temp->next = NULL; 
+  
+    return temp; 
+} 
+
+Queue* createQueue( int numberOfFrames ) { 
+    Queue* queue = (Queue *)malloc( sizeof( Queue ) ); 
+  
+    queue->count = 0; 
+    queue->front = queue->rear = NULL; 
+  
+    queue->numberOfFrames = numberOfFrames; 
+  
+    return queue; 
+} 
+
+Hash* createHash( int capacity){ 
+    // Allocate memory for hash 
+    Hash* hash = (Hash *) malloc( sizeof( Hash ) ); 
+    hash->capacity = capacity; 
+  
+    // Create an array of pointers for refering queue nodes 
+    hash->array = (QNode **) malloc( hash->capacity * sizeof( QNode* ) ); 
+  
+    // Initialize all hash entries as empty 
+    int i; 
+    for( i = 0; i < hash->capacity; ++i ) 
+        hash->array[i] = NULL; 
+  
+    return hash; 
+} 
+
+// A function to check if there is slot available in memory 
+int AreAllFramesFull( Queue* queue){ 
+    return queue->count == queue->numberOfFrames; 
+} 
+  
+// A utility function to check if queue is empty 
+int isQueueEmpty( Queue* queue){ 
+    return queue->rear == NULL; 
+}
+
+// A utility function to delete a frame from queue 
+void deQueue( Queue* queue){ 
+    if( isQueueEmpty( queue ) ) 
+        return; 
+  
+    // If this is the only node in list, then change front 
+    if (queue->front == queue->rear) 
+        queue->front = NULL; 
+  
+    // Change rear and remove the previous rear 
+    QNode* temp = queue->rear; 
+    queue->rear = queue->rear->prev; 
+  
+    if (queue->rear) 
+        queue->rear->next = NULL; 
+  
+    free( temp ); 
+  
+    // decrement the number of full frames by 1 
+    queue->count--; 
+}
+
+void Enqueue( Queue* queue, Hash* hash, unsigned pageNumber){ 
+    if (AreAllFramesFull(queue)){ 
+        hash->array[ queue->rear->pageNumber ] = NULL; 
+        deQueue( queue ); 
+    } 
+  
+    QNode* temp = newQNode( pageNumber ); 
+    temp->next = queue->front; 
+  
+    if ( isQueueEmpty( queue ) ) 
+        queue->rear = queue->front = temp; 
+    else{ 
+        queue->front->prev = temp; 
+        queue->front = temp; 
+    } 
+  
+    hash->array[ pageNumber ] = temp; 
+  
+    queue->count++; 
+} 
+
+void LRU(Queue* queue, Hash* hash, unsigned pageNumber){ 
+    QNode* reqPage = hash->array[ pageNumber ]; 
+  
+    if ( reqPage == NULL ){
+        Enqueue( queue, hash, pageNumber );
+        tlbMissCount++;
+
+    }else if (reqPage != queue->front){ 
+        tlbHitCount++;
+        reqPage->prev->next = reqPage->next; 
+        if (reqPage->next) 
+           reqPage->next->prev = reqPage->prev; 
+  
+        if (reqPage == queue->rear){ 
+           queue->rear = reqPage->prev; 
+           queue->rear->next = NULL; 
+        } 
+  
+        reqPage->next = queue->front; 
+        reqPage->prev = NULL; 
+  
+        reqPage->next->prev = reqPage; 
+  
+        queue->front = reqPage; 
+    } 
+} 
+
+void FIFO(Queue* queue, Hash* hash, unsigned pageNumber){ 
+    QNode* reqPage = hash->array[ pageNumber ]; 
+  
+    if ( reqPage == NULL ){
+        tlbMissCount++;
+        Enqueue( queue, hash, pageNumber );
+
+    }else if (reqPage != NULL){ 
+        tlbHitCount++;
+    } 
+} 
 
 
 int main(int argc, char *argv[]){
@@ -42,7 +184,7 @@ int main(int argc, char *argv[]){
                 break;
             case 'f': 
                 if (f_flag == 0){
-                    flushPeriod = optarg;
+                    flushPeriod = atoi(optarg);
                 }
                 f_flag++;
                 break;
@@ -67,11 +209,75 @@ int main(int argc, char *argv[]){
 
     //simulate the TLB based FIFO or LRU policy.
     if(strcmp(policy,"FIFO")==0){
-        int key = tblsize;
-        //set capacity to 16 for now
-        LinkedHashMap* linkedHashMap = createLinkedHashMap(16, 0.5);
-        //put(linkedHashMap, key, 56);
-        //Element* element = get(linkedHashMap,key);
+        // The queue is the TLB table so set the queue size to the tlbsize 
+        Queue* queue = createQueue(tblsize); 
+  
+        //Possible number of pages is the hash for quick lookups.
+        Hash* hash = createHash(100000);
+        
+        int flushCount =0;
+        
+        while (fgets(buf, sizeof(buf), stdin)) {
+            if (buf[0] == '=')
+                continue;
+            if (i_flag == 1){
+                if (buf[0] != 'I'){
+                    if (f_flag >0){
+                        if (flushPeriod == flushCount){
+                            while(!isQueueEmpty(queue)){
+                                deQueue(queue);
+                            }
+                            int i; 
+                            for( i = 0; i < hash->capacity; ++i ) 
+                                hash->array[i] = NULL; 
+                        }
+                    }
+                    strncpy(addr_str, buf + 3, 8);
+                    sscanf(addr_str, "%x", &addr);
+                    unsigned int pageNumber = addr >> pgsize_nbits;
+                    //printf("%x\n", pageNumber);
+                    FIFO(queue,hash,pageNumber);
+                    
+                    if(flushCount != flushPeriod){
+                        flushCount++;
+                    } else if(flushCount == flushPeriod){
+                        flushCount =0;
+                    }
+                    
+                    memset(buf, 0, 100);
+                }
+            }else{
+                if (f_flag >0){
+                    if (flushPeriod == flushCount){
+                        while(!isQueueEmpty(queue)){
+                            deQueue(queue);
+                        }
+                        int i; 
+                        for( i = 0; i < hash->capacity; ++i ) 
+                            hash->array[i] = NULL; 
+                    }
+                }
+        
+                strncpy(addr_str, buf + 3, 8);
+                sscanf(addr_str, "%x", &addr);
+                unsigned int pageNumber = addr >> pgsize_nbits;
+                FIFO(queue,hash,pageNumber);
+                //printf("%x\n", pageNumber);
+
+                if(flushCount != flushPeriod){
+                    flushCount++;
+                } else if(flushCount == flushPeriod){
+                    flushCount =0;
+                }
+                memset(buf, 0, 100);
+            }
+        }
+    }else if(strcmp(policy,"LRU")==0){
+        // The queue is the TLB table so set the queue size to the tlbsize 
+        Queue* queue = createQueue(tblsize); 
+  
+        //Possible number of pages is the hash for quick lookups.
+        Hash* hash = createHash(100000); 
         
         while (fgets(buf, sizeof(buf), stdin)) {
             if (buf[0] == '=')
@@ -80,76 +286,30 @@ int main(int argc, char *argv[]){
                 if (buf[0] != 'I'){
                     strncpy(addr_str, buf + 3, 8);
                     sscanf(addr_str, "%x", &addr);
-                    addr = addr >> pgsize_nbits;
-                    printf("%d\n",addr);
-            
-                    put(linkedHashMap, addr, 1);
-                    //Element* element = get(linkedHashMap,key);
-                    //printf("%x\n",element->value);
-                    //Element* element2 = get(linkedHashMap,tblsize);
-                    //printf("THE TBLSIZE KEY: %x\n",element2->value);
+                    unsigned int pageNumber = addr >> pgsize_nbits;
+                    LRU(queue,hash,pageNumber);
+                    //printf ("%x\n", queue->front->pageNumber);
 
 
-                    
-                    
-                    
-                    while(key>0){
-                        key--;
-                    }
-                    if(key == 0){
-                        delete(linkedHashMap,key);
-                    }
-                    //key++;
+        
                     memset(buf, 0, 100);
                 }
-            
-            
-            
-            
             
             }else{
                 strncpy(addr_str, buf + 3, 8);
                 sscanf(addr_str, "%x", &addr);
-                addr = addr >> pgsize_nbits;
-                printf("%x\n", addr);
+                unsigned int pageNumber = addr >> pgsize_nbits;
+                LRU(queue,hash,pageNumber);
+                //printf ("%x\n", queue->front->pageNumber);
+                
                 memset(buf, 0, 100);
+                
             }
         }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    }else if(strcmp(policy,"LRU")==0){
-        while (fgets(buf, sizeof(buf), stdin)) {
-            if (buf[0] == '=')
-                continue;
-            if (i_flag == 1){
-                if (buf[0] != 'I'){
-                    strncpy(addr_str, buf + 3, 8);
-                    sscanf(addr_str, "%x", &addr);
-                    addr = addr >> pgsize_nbits;
-                    printf("%x\n", addr);
-                    memset(buf, 0, 100);
-                }
-            }else{
-                strncpy(addr_str, buf + 3, 8);
-                sscanf(addr_str, "%x", &addr);
-                addr = addr >> pgsize_nbits;
-                printf("%x\n", addr);
-                memset(buf, 0, 100);
-            }
-        }
-    
-    
-    
     
     }
+    printf("Hit Count: %d\n",tlbHitCount);
+    printf("Miss Count: %d\n",tlbMissCount);
 
     return 0;
 }
