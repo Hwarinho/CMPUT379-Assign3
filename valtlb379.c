2f91315047162275e0d01115d019f6d5f8caf78f
Oleg Krushelnytskyy <oleg@ualberta.ca>
2019-04-09 22:48:21 -0600

    Update valtlb379.c

--- valtlb379.c
+++ valtlb379.c
@@ -12,11 +12,13 @@
 
 /*
  * Our Translation Look-aside Buffer Simulator can be run with the arguments: 
- * valgrind --tool=lackey --trace-mem=yes ./outputProgram 2>&1 | ./valtlb379 -i -f (flush pero-p Po 16 32
+ * valgrind --tool=lackey --trace-mem=yes ./inputProgram 2>&1 | ./valtlb379 -i -f (flush period) -p (Policy) pagesize tablesize
+ * INSTRUCTIONS: -i flag removes all instructions from feed; -f flush flushes the data structure every n references; policy uses either a FIFO or LRU policy on table;
+ * pagesize is a power of 2 (from 16 to 65,536); tablesize is number of page table entries in (always a power of 2 and no more than 2,048)
  * 
- * 
- * 
-*/
+ * We implemented a Double linked list as a queue for keeping order of our page numbers and a Hash for quick lookup.
+ * Our code allows the data structure to function in a FIFO or least recently used manner.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,6 +27,7 @@
 #include <getopt.h>
 #include <math.h>
 
+//define variables
 const char* policy;
 int flushPeriod;
 int pgsize;
@@ -33,56 +36,57 @@ int totalReferences=0;
 int totalReferences_i=0;
 int tlbHitCount=0;
 int tlbMissCount=0;
-
 char buf[100] = {0};
 char addr_str[9] = {0};
 unsigned int addr;
 
-// A Queue Node (Queue is implemented using Doubly Linked List) 
+// A Queue Node that is implemented using Double Linked List
 typedef struct QNode{ 
     struct QNode *prev, *next; 
     unsigned pageNumber;
 } QNode; 
 
-// A Queue (A FIFO collection of Queue Nodes) 
+// define Queue
 typedef struct Queue{ 
     unsigned count;  
     unsigned numberOfFrames; 
     QNode *front, *rear; 
 } Queue; 
   
-// A hash (Collection of pointers to Queue Nodes) 
+// define Hash
 typedef struct Hash{ 
     int capacity; 
     QNode* *array;
 } Hash;
 
+// Queue node that will store a pageNumber
 QNode* newQNode( unsigned pageNumber){ 
+    // allocates memory and assigns pageNumber
     QNode* temp = (QNode *)malloc( sizeof( QNode ) ); 
     temp->pageNumber = pageNumber; 
-
+    // next and previous is NULL
     temp->prev = temp->next = NULL; 
   
     return temp; 
 } 
 
+// Initialize an empty queue
 Queue* createQueue( int numberOfFrames ) { 
     Queue* queue = (Queue *)malloc( sizeof( Queue ) ); 
-  
     queue->count = 0; 
     queue->front = queue->rear = NULL; 
-  
+    // capacity of queue
     queue->numberOfFrames = numberOfFrames; 
   
     return queue; 
 } 
 
+// Initialize Hash table
 Hash* createHash( int capacity){ 
-    // Allocate memory for hash 
     Hash* hash = (Hash *) malloc( sizeof( Hash ) ); 
     hash->capacity = capacity; 
   
-    // Create an array of pointers for refering queue nodes 
+    // Link hash to queue
     hash->array = (QNode **) malloc( hash->capacity * sizeof( QNode* ) ); 
   
     // Initialize all hash entries as empty 
@@ -103,7 +107,7 @@ int isQueueEmpty( Queue* queue){
     return queue->rear == NULL; 
 }
 
-// A utility function to delete a frame from queue 
+// Delete a node from queue
 void deQueue( Queue* queue){ 
     if( isQueueEmpty( queue ) ) 
         return; 
@@ -121,19 +125,23 @@ void deQueue( Queue* queue){
   
     free( temp ); 
   
-    // decrement the number of full frames by 1 
+    // Decrement the number of full frames by 1 
     queue->count--; 
 }
 
+// Puts a new element in queue and adds its page number to the hash
 void Enqueue( Queue* queue, Hash* hash, unsigned pageNumber){ 
+    //if no spot available in memory, remove the rear in queue and clear its corresponding hash value.
     if (AreAllFramesFull(queue)){ 
         hash->array[ queue->rear->pageNumber ] = NULL; 
-        deQueue( queue ); 
+        deQueue(queue); 
     } 
-  
+    
+    //make temp node to put in the front
     QNode* temp = newQNode( pageNumber ); 
     temp->next = queue->front; 
-  
+    
+    //sets temp to front
     if ( isQueueEmpty( queue ) ) 
         queue->rear = queue->front = temp; 
     else{ 
@@ -141,18 +149,22 @@ void Enqueue( Queue* queue, Hash* hash, unsigned pageNumber){
         queue->front = temp; 
     } 
   
+    //input new nodes pageNumber into the Hash
     hash->array[ pageNumber ] = temp; 
   
     queue->count++; 
 } 
 
+// LRU checks for node in queue, if it isn't in the queue, it deletes the least recently used node and inserts the new node to the front of the queue
 void LRU(Queue* queue, Hash* hash, unsigned pageNumber){ 
+    //check if the pageNumber of the node exists in the hash.
     QNode* reqPage = hash->array[ pageNumber ]; 
-  
+
+    //if not in queue, add to the top; TLB Miss
     if ( reqPage == NULL ){
         Enqueue( queue, hash, pageNumber );
         tlbMissCount++;
-
+    //if not in the front of the queue, move to the front; TLB Hit
     }else if (reqPage != queue->front){ 
         tlbHitCount++;
         reqPage->prev->next = reqPage->next; 
@@ -170,32 +182,36 @@ void LRU(Queue* queue, Hash* hash, unsigned pageNumber){
         reqPage->next->prev = reqPage; 
   
         queue->front = reqPage; 
+    //if at the front of the queue, keep it there; TLB Hit
     } else if (reqPage == queue->front){
         tlbHitCount++;
     }
 } 
 
+// FIFO Enqueue's the new element if not in the queue and adds a miss, if it is in the queue, it simply records hit and moves on.
 void FIFO(Queue* queue, Hash* hash, unsigned pageNumber){ 
+    // checks if the nodes pageNumber exists in the Hash
     QNode* reqPage = hash->array[ pageNumber ]; 
   
+    // if page is not in queue, enqueue it; TLB Miss
     if ( reqPage == NULL ){
         tlbMissCount++;
         Enqueue( queue, hash, pageNumber );
-
+    // if page is in queue, leave it; TLB Hit
     }else if (reqPage != NULL){ 
         tlbHitCount++;
     } 
 } 
 
-
+// Main takes in arguments, raises flags and stores info, then it performs FIFO and LRU depending on the arguments
 int main(int argc, char *argv[]){
-    //Identify the flags specified by the user and obtain their respective inputs.
+    //input flags
     int flag;
     int i_flag =0;
     int f_flag =0;
     int p_flag =0;
     
-
+    //getopt on arguments in order to raise flags that the user is asking for certain features.
     while ((flag = getopt(argc, argv, "if:p:")) != -1){
         switch(flag){
             case 'i':
@@ -222,8 +238,8 @@ int main(int argc, char *argv[]){
     pgsize = atoi(argv[argc-2]);
     tblsize = atoi(argv[argc-1]);
 
+    //does calculation for how many bits we will have to shift depending on page size
     const unsigned pgsize_nbits = log(pgsize)/log(2);
-    //printf("%d",pgsize_nbits);
 
     //not enough arguments provided by user.
     if ((argc <5) || (p_flag ==0)){
@@ -231,7 +247,7 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
-    //simulate the TLB based FIFO or LRU policy.
+    //simulate the TLB based FIFO policy.
     if(strcmp(policy,"FIFO")==0){
         // The queue is the TLB table so set the queue size to the tlbsize 
         Queue* queue = createQueue(tblsize); 
@@ -242,15 +258,20 @@ int main(int argc, char *argv[]){
         int flushCount =0;
         char *last_char;
         
+        //streams value from inputProgram and seperate based on w/ instructions or without.
         while (fgets(buf, sizeof(buf), stdin)) {
             totalReferences++;
+            //stops while loop as soon as you see '=' lines
             if (buf[0] == '=')
                 continue;
+            //if instruction flag is raised and we don't count instructions
             if (i_flag == 1){
                 if (buf[0] != 'I'){
                     totalReferences_i++;
+                    //if f flag is raised and we must keep count when to flush TLB
                     if (f_flag >0){
                         if (flushPeriod == flushCount){
+                            //flushes TLB
                             while(!isQueueEmpty(queue)){
                                 deQueue(queue);
                             }
@@ -259,6 +280,7 @@ int main(int argc, char *argv[]){
                                 hash->array[i] = NULL; 
                         }
                     }
+                    //obtains line from valgrind, cuts off the string after ',' and does bit shifting based on pgsize.
                     strncpy(addr_str, buf + 3, 50);
 
                     last_char = strchr(addr_str, ',');
@@ -268,18 +290,22 @@ int main(int argc, char *argv[]){
 
                     sscanf(addr_str, "%x", &addr);
                     unsigned int pageNumber = addr >> pgsize_nbits;
+                    //make sure the page number will fit into a bucket in our modulo Hash table
                     pageNumber = pageNumber % 1000003;
+                    //call FIFO with this pageNumber (simulate TLB)
                     FIFO(queue,hash,pageNumber);
                     
+                    //incrementer for flush period.
                     if(flushCount != flushPeriod){
                         flushCount++;
                     } else if(flushCount == flushPeriod){
                         flushCount =0;
                     }
-                    
+                    //clear memory
                     memset(buf, 0, 100);
                 }
             }else{
+                //does the same thing as FIFO above but w/ instruction references included
                 if (f_flag >0){
                     if (flushPeriod == flushCount){
                         while(!isQueueEmpty(queue)){
@@ -310,22 +336,26 @@ int main(int argc, char *argv[]){
                 memset(buf, 0, 100);
             }
         }
+    //simulate TLB using Least Recently Used method    
     }else if(strcmp(policy,"LRU")==0){
-        // The queue is the TLB table so set the queue size to the tlbsize 
+        //similar to FIFO above
         Queue* queue = createQueue(tblsize); 
   
-        //Possible number of buckets in our modulo hash that the value might belong to.
         Hash* hash = createHash(1000003); 
 
         int flushCount =0;
         char *last_char;
         
+        //gets string from pipe from inputProgram w/ instructions or without
         while (fgets(buf, sizeof(buf), stdin)) {
             totalReferences++;
+            //stops while loop as soon as you reach "=" from valgrind
             if (buf[0] == '=')
                 continue;
+            //checks i flag in order to ignore or not ignore instruction references.
             if (i_flag == 1){
                 if (buf[0] != 'I'){
+                    //EXACTLY the same as for FIFO above but calls LRU instead of FIFO
                     totalReferences_i++;
                     if (f_flag >0){
                         if (flushPeriod == flushCount){
@@ -360,6 +390,7 @@ int main(int argc, char *argv[]){
                 }
             
             }else{
+                //EXACTLY the same as above but include instruction references.
                 if (f_flag >0){
                     if (flushPeriod == flushCount){
                         while(!isQueueEmpty(queue)){
@@ -381,7 +412,6 @@ int main(int argc, char *argv[]){
                 unsigned int pageNumber = addr >> pgsize_nbits;
                 pageNumber = pageNumber % 1000003;
                 LRU(queue,hash,pageNumber);
-                //printf ("%x\n", queue->front->pageNumber);
 
                 if(flushCount != flushPeriod){
                     flushCount++;
@@ -396,6 +426,7 @@ int main(int argc, char *argv[]){
     
     }
 
+    // printing of results to stdout
     printf("\n");
     if(i_flag >0){
         printf("Total Reference: %d\n",totalReferences_i);    
@@ -404,15 +435,7 @@ int main(int argc, char *argv[]){
     }
     printf("Miss Count: %d\n",tlbMissCount);
     printf("Hit Count: %d\n",tlbHitCount);
-
-    float miss_rate;
-
-    if(i_flag>0){
-        miss_rate = ((float) tlbMissCount/(float)totalReferences_i) *100;
-    }else if(i_flag==0){
-        miss_rate = ((float) tlbMissCount/(float) totalReferences) *100;
-    }   
-    printf("Miss rate: %f\n",miss_rate);
+    printf("\n");
 
     return 0;
 }
